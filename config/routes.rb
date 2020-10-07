ffb008f31f60c1327fc0f31d403730bd3e243023
Zach Stearns <zach.stearns92@gmail.com>
2020-10-06 19:03:29 -0600

    Reorder lines

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get '/', to: 'welcome#index'
   get '/shelters', to: 'shelters#index'
-  get '/shelters/:id', to: 'shelters#show'
   get '/shelters/new', to: 'shelters#new'
   post '/shelters', to: 'shelters#create'
+  get '/shelters/:id', to: 'shelters#show'
 end
