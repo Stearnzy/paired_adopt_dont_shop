ed5261bdd5b8fe75567fddbe55d63970fe03d472
Zach Stearns <zach.stearns92@gmail.com>
2020-10-06 19:04:38 -0600

    Setup new and create methods

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -2,4 +2,11 @@ class SheltersController < ApplicationController
   def index
     @shelters = ['Dumb Friends League', 'Max Fund', 'House for Cats']
   end
+
+  def new
+  end
+
+  def create
+
+  end
 end
