eb402e9f6f0eddaaa8c16471b42bdb91f0f5e691
Zach Stearns <zach.stearns92@gmail.com>
2020-10-08 17:38:51 -0600

    Correctly implement before each

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -2,48 +2,36 @@ require 'rails_helper'
 
 describe "As a visitor" do
   describe "when I visit '/shelters/:id'" do
+    before(:each) do
+      @shelter = Shelter.create({
+        name: "Crazy Cat Lady's",
+        address: "123 Litterbox Way",
+        city: "Littleton",
+        state: "CO",
+        zip: "80110"
+        })
+
+      end
+
     it "I see the shelter with that id including the shelter's
           name, address, city, state, and zip" do
-      shelter = Shelter.create({
-            name: "Crazy Cat Lady's",
-            address: "123 Litterbox Way",
-            city: "Littleton",
-            state: "CO",
-            zip: "80110"
-        })
-      visit "/shelters/#{shelter.id}"
-
+      visit "/shelters/#{@shelter.id}"
+      
       expect(page).to have_link("Shelter Index")
-      expect(page).to have_content("#{shelter.name}")
-      expect(page).to have_content("#{shelter.address}")
-      expect(page).to have_content("#{shelter.city}")
-      expect(page).to have_content("#{shelter.state}")
-      expect(page).to have_content("#{shelter.zip}")
+      expect(page).to have_content("#{@shelter.name}")
+      expect(page).to have_content("#{@shelter.address}")
+      expect(page).to have_content("#{@shelter.city}")
+      expect(page).to have_content("#{@shelter.state}")
+      expect(page).to have_content("#{@shelter.zip}")
     end
 
     it "I see a link to update the shelter" do
-      shelter = Shelter.create({
-            name: "Crazy Cat Lady's",
-            address: "123 Litterbox Way",
-            city: "Littleton",
-            state: "CO",
-            zip: "80110"
-        })
-      visit "/shelters/#{shelter.id}"
-
+      visit "/shelters/#{@shelter.id}"
       expect(page).to have_link("Update Shelter")
     end
 
     it "I see a link to delete the shelter" do
-      shelter = Shelter.create({
-            name: "Crazy Cat Lady's",
-            address: "123 Litterbox Way",
-            city: "Littleton",
-            state: "CO",
-            zip: "80110"
-        })
-      visit "/shelters/#{shelter.id}"
-
+      visit "/shelters/#{@shelter.id}"
       expect(page).to have_button("Delete")
     end
   end
