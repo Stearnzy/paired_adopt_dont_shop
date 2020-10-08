07dc0d41d1cbd26e5481b0950e359834d5adec07
Zach Stearns <zach.stearns92@gmail.com>
2020-10-07 22:12:06 -0600

    Add expectation for heading

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -15,6 +15,8 @@ describe "As a visitor" do
 
       visit '/pets'
 
+      expect(page).to have_content("Furry Friends Needing a Home")
+
       expect(page).to have_content("https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074__340.jpg")
       expect(page).to have_content("Guiness")
       expect(page).to have_content(3)
