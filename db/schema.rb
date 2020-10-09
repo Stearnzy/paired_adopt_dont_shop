75366aa0b688e0ea4540d5aa75f7dd6731d6957a
Zach Stearns <zach.stearns92@gmail.com>
2020-10-08 20:03:26 -0600

    Add column to pets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_08_040717) do
+ActiveRecord::Schema.define(version: 2020_10_09_020249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_10_08_040717) do
     t.integer "age"
     t.string "sex"
     t.string "shelter"
+    t.string "status"
   end
 
   create_table "shelters", force: :cascade do |t|
