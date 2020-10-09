9de0538c0aacc7621ea1d78e728742ef86127503
Zach Stearns <zach.stearns92@gmail.com>
2020-10-08 21:37:10 -0600

    Migrate connection between tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_025618) do
+ActiveRecord::Schema.define(version: 2020_10_09_033422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_10_09_025618) do
     t.string "sex"
     t.string "current_shelter"
     t.string "status"
+    t.bigint "shelter_id"
+    t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
   create_table "shelters", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema.define(version: 2020_10_09_025618) do
     t.string "zip"
   end
 
+  add_foreign_key "pets", "shelters"
 end
