d0c7165cd52bd02af95ac7bf6dc8a6bb2ac3bfa8
Zach Stearns <zach.stearns92@gmail.com>
2020-10-09 17:39:47 -0600

    Alter tests and variable to correct format

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -5,24 +5,33 @@ describe "As a visitor" do
     it "I see each Pet in the system including image, name, approximate age,
         sex, and name of shelter where pet is currently located" do
 
-        visit '/pets'
+      shelter = Shelter.create({
+            name: "Dog's Haven",
+            address: "123 Litterbox Way",
+            city: "Littleton",
+            state: "CO",
+            zip: "80110"
+        })
+
       pet_1 = Pet.create({
         image: "https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074__340.jpg",
         name: "Guiness",
         age: 3,
         sex: "male",
-        current_shelter: "Dog's Haven"
-        })
+        current_shelter: "#{shelter.name}",
+        shelter_id: "#{shelter.id}",
+        status: "adoptable"
+      })
 
+        visit '/pets'
 
-      # expect(page).to have_content("Furry Friends Needing a Home")
+      expect(page).to have_content("Furry Friends Needing a Home")
 
-      # expect(page).to have_xpath("//img[contains(@src, '#{pet_1.image}')]")
+      expect(page).to have_xpath("//img[contains(@src, '#{pet_1.image}')]")
       expect(page).to have_content("#{pet_1.name}")
       expect(page).to have_content("#{pet_1.age}")
       expect(page).to have_content("#{pet_1.sex}")
       expect(page).to have_content("#{pet_1.current_shelter}")
     end
-
   end
 end
