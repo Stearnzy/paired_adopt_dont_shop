20426ceb09311883381958116151dd9ddb646889
Zach Stearns <zach.stearns92@gmail.com>
2020-10-09 20:32:59 -0600

    Remove status column from pets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_005933) do
+ActiveRecord::Schema.define(version: 2020_10_10_023129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2020_10_10_005933) do
     t.string "name"
     t.integer "age"
     t.string "sex"
-    t.string "status"
     t.bigint "shelter_id"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
