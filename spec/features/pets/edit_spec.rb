f9e53e16c703a05a62b3a648825838c3cc17825e
Zach Stearns <zach.stearns92@gmail.com>
2020-10-10 16:21:37 -0600

    Add before each for pet and shelter, add click link test

--- spec/features/pets/edit_spec.rb
+++ spec/features/pets/edit_spec.rb
@@ -1,3 +1,35 @@
+require 'rails_helper'
+
+describe "As a visitor" do
+  describe "When I visit a Pet Show page, Then I see a link to update that pet" do
+    before(:each) do
+      @shelter = Shelter.create({
+        name: "Perky Parakeets",
+        address: "666 Tropicana Way",
+        city: "Las Vegas",
+        state: "NV",
+        zip: "66666"
+        })
+
+      @pet = Pet.create({
+        image: "https://media.angieslist.com/s3fs-public/styles/widescreen_large/public/parakeet.jpeg?itok=2pMpm55J",
+        name: "Perry",
+        description: "Beautiful golden bird, ready to party!",
+        age: 7,
+        sex: "Female",
+        shelter_id: "#{@shelter.id}"
+        })
+    end
+
+    it "When I click the link I am taken to '/pets/:id/edit' where I see a form" do
+
+      visit "/pets/#{@pet.id}"
+      click_link "Update Pet"
+      expect(current_path).to eq("/pets/#{@pet.id}/edit")
+    end
+  end
+end
+
 # As a visitor
 # When I visit a Pet Show page
 # Then I see a link to update that Pet "Update Pet"
