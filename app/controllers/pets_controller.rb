85c7316933cd6015b9ffa1ea3cebc9ea270cb1b9
Zach Stearns <zach.stearns92@gmail.com>
2020-10-10 15:40:33 -0600

    Add create action, add shelter definition to new action

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,6 +4,22 @@ class PetsController < ApplicationController
   end
 
   def new
+    @shelter = Shelter.find(params[:shelter_id])
+  end
+
+  def create
+    pet = Pet.create({
+      image: params[:image],
+      name: params[:name],
+      description: params[:description],
+      age: params[:age],
+      sex: params[:sex],
+      shelter_id: params[:shelter_id]
+      })
+
+    pet.save
+
+    redirect_to "/shelters/#{pet.shelter_id}/pets"
   end
 
   def show
