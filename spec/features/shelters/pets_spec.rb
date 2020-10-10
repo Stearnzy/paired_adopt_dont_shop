01357eed920cf331666bab14040a3b84f1387385
Zach Stearns <zach.stearns92@gmail.com>
2020-10-10 00:26:15 -0600

    Remove before each block, add have link create pet expectation

--- spec/features/shelters/pets_spec.rb
+++ spec/features/shelters/pets_spec.rb
@@ -2,18 +2,17 @@ require 'rails_helper'
 
 describe "As a visitor" do
   describe "When I visit '/shelters/:shelter_id/pets'" do
-    before(:each) do
-      @shelter_1 = Shelter.create({
+    it "then I see each Pet that can be adopted from that shelter with that shelter_id
+        including the pet's image, name, appoximate age, and sex" do
+
+      shelter_1 = Shelter.create({
             name: "Crazy Cat Lady's",
             address: "123 Litterbox Way",
             city: "Littleton",
             state: "CO",
             zip: "80110"
         })
-    end
 
-    it "then I see each Pet that can be adopted from that shelter with that shelter_id
-        including the pet's image, name, appoximate age, and sex" do
 
       pet_1 = Pet.create({
         image: "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg",
@@ -21,29 +20,18 @@ describe "As a visitor" do
         description: "Fat happy cat wants some lasagna.",
         age: 5,
         sex: "Male",
-        shelter_id: "#{@shelter_1.id}"
+        shelter_id: "#{shelter_1.id}"
         })
 
-      visit "/shelters/#{@shelter_1.id}/pets"
+      visit "/shelters/#{shelter_1.id}/pets"
 
-      expect(page).to have_content("#{@shelter_1.name}: Pets")
+      expect(page).to have_content("#{shelter_1.name}: Pets")
       expect(page).to have_xpath("//img[contains(@src, '#{pet_1.image}')]")
       expect(page).to have_content("#{pet_1.name}")
       expect(page).to have_content(pet_1.age)
       expect(page).to have_content("#{pet_1.sex}")
+
+      expect(page).to have_link("Create Pet")
     end
-
   end
-
-  # describe "Then I see a link to add a new adoptable pet for the shelter 'Create Pet'" do
-  #   it "When I click the link I am taken to a new page where there is a form to
-  #       add a new adoptable pet; enter image, name, description, age, sex and click
-  #       create pet.  Pet is posted to that shelter" do
-  #
-  #         visit "/shelters/#{shelter_1.id}/pets"
-  #
-  #         click_link("Create Pet")
-  #         expect(current_path).to eq("/shelters/#{shelter_1.id}/pets/new")
-  #       end
-  # end
 end
