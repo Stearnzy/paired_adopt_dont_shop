026c3c017c3bae90c11de447ed0aad9d4b635b7a
Zach Stearns <zach.stearns92@gmail.com>
2020-10-09 18:29:28 -0600

    Remove unnecessary attributs

--- spec/features/shelters/pets_spec.rb
+++ spec/features/shelters/pets_spec.rb
@@ -1,19 +1,11 @@
 require 'rails_helper'
 
-# As a visitor
-# When I visit '/shelters/:shelter_id/pets'
-# Then I see each Pet that can be adopted from that Shelter with that shelter_id including the Pet's:
-# - image
-# - name
-# - approximate age
-# - sex
-
 describe "As a visitor" do
   describe "When I visit '/shelters/:shelter_id/pets'" do
     it "then I see each Pet that can be adopted from that shelter with that shelter_id
         including the pet's image, name, appoximate age, and sex" do
 
-        shelter = Shelter.create({
+        shelter_1 = Shelter.create({
               name: "Crazy Cat Lady's",
               address: "123 Litterbox Way",
               city: "Littleton",
@@ -21,12 +13,21 @@ describe "As a visitor" do
               zip: "80110"
           })
 
-        pet_1 = Pet.create({
+        shelter_2 = Shelter.create({
+              name: "Dog's Haven",
+              address: "123 Litterbox Way",
+              city: "Littleton",
+              state: "CO",
+              zip: "80110"
+          })
+
+
+        pet_1 = Pet.create!({
           image: "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg",
           name: "Garfield",
           age: 5,
           sex: "male",
-          current_shelter: "Crazy Cat Lady's"
+          shelter_id: "#{shelter_1.id}"
           })
 
         pet_2 = Pet.create({
@@ -34,20 +35,19 @@ describe "As a visitor" do
           name: "Guiness",
           age: 3,
           sex: "male",
-          current_shelter: "Dog's Haven"
+          shelter_id: "#{shelter_2.id}"
           })
 
 
-      visit "/shelters/#{shelter.id}/pets"
+      visit "/shelters/#{shelter_1.id}/pets"
 
-      expect(page).to have_content("#{shelter.name}: Pets")
+      expect(page).to have_content("#{shelter_1.name}: Pets")
       expect(page).to have_xpath("//img[contains(@src, '#{pet_1.image}')]")
       expect(page).to have_content("#{pet_1.name}")
-      # May need to change this, integer
       expect(page).to have_content(pet_1.age)
       expect(page).to have_content("#{pet_1.sex}")
 
-      expect(page).to have_link("Return to #{shelter.name}")
+      expect(page).to have_link("Return to #{shelter_1.name}")
       expect(page).to have_link("Return to Shelters Home")
     end
   end
