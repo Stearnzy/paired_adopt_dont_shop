a796344e18491bcb6925c9b8e473cb93a3daf848
Zach Stearns <zach.stearns92@gmail.com>
2020-10-10 15:39:07 -0600

    Update test for new correctly

--- spec/features/pets/new_spec.rb
+++ spec/features/pets/new_spec.rb
@@ -5,7 +5,7 @@ describe "Then I see a link to add a new adoptable pet for the shelter 'Create P
       add a new adoptable pet; enter image, name, description, age, sex and click
       create pet.  Pet is posted to that shelter" do
 
-        @shelter = Shelter.create({
+        shelter = Shelter.create({
           name: "Crazy Cat Lady's",
           address: "123 Litterbox Way",
           city: "Littleton",
@@ -13,10 +13,11 @@ describe "Then I see a link to add a new adoptable pet for the shelter 'Create P
           zip: "80110"
           })
 
-        visit "/shelters/#{@shelter.id}/pets"
+        visit "/shelters/#{shelter.id}/pets"
 
         click_link("Create Pet")
-        expect(current_path).to eq("/shelters/#{@shelter.id}/pets/new")
+        expect(current_path).to eq("/shelters/#{shelter.id}/pets/new")
+        expect(page).to have_content("Create New Pet:")
 
         # attach_file("image", "https://frontpagemeews.com/wp-content/uploads/2018/12/lazy-cat-cover-750x517.png")
         fill_in "image", with: "https://frontpagemeews.com/wp-content/uploads/2018/12/lazy-cat-cover-750x517.png"
@@ -27,13 +28,11 @@ describe "Then I see a link to add a new adoptable pet for the shelter 'Create P
 
         click_button 'Create Pet'
 
-        expect(current_path).to eq("/shelters/#{@shelter.id}/pets")
-        expect(page).to have_content("Create New Pet:")
+        expect(current_path).to eq("/shelters/#{shelter.id}/pets")
         expect(page).to have_xpath("//img[contains(@src, 'https://frontpagemeews.com/wp-content/uploads/2018/12/lazy-cat-cover-750x517.png')]")
-        expect(page).to have_content("#{@shelter.name}: Pets")
+        expect(page).to have_content("#{shelter.name}: Pets")
         expect(page).to have_content("Opus")
-        expect(page).to have_content("He's a quiet guy.")
         expect(page).to have_content(7)
-        expect(find_field("Sex")).to be_checked
+        expect(page).to have_content("Male")
   end
 end
