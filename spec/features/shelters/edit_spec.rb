ec932113e56235dd153a8513295ce74644216331
Zach Stearns <zach.stearns92@gmail.com>
2020-10-10 18:26:02 -0600

    Add test for updated info on show page

--- spec/features/shelters/edit_spec.rb
+++ spec/features/shelters/edit_spec.rb
@@ -28,6 +28,12 @@ describe "As a user" do
 
     click_button('submit')
     expect(current_path).to eq("/shelters/#{shelter.id}")
+
+    expect(page).to have_content("#{shelter.name}")
+    expect(page).to have_content("#{shelter.address}")
+    expect(page).to have_content("Denver")
+    expect(page).to have_content("#{shelter.state}")
+    expect(page).to have_content("#{shelter.zip}")
     end
   end
 end
