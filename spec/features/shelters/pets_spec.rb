e0c25b294746db4114e37f375658fa2a77defef8
Zach Stearns <zach.stearns92@gmail.com>
2020-10-10 20:18:34 -0600

    Add before each and fix tests

--- spec/features/shelters/pets_spec.rb
+++ spec/features/shelters/pets_spec.rb
@@ -2,10 +2,8 @@ require 'rails_helper'
 
 describe "As a visitor" do
   describe "When I visit '/shelters/:shelter_id/pets'" do
-    it "then I see each Pet that can be adopted from that shelter with that shelter_id
-        including the pet's image, name, appoximate age, and sex" do
-
-      shelter_1 = Shelter.create({
+    before(:each) do
+      @shelter_1 = Shelter.create({
             name: "Crazy Cat Lady's",
             address: "123 Litterbox Way",
             city: "Littleton",
@@ -13,23 +11,26 @@ describe "As a visitor" do
             zip: "80110"
         })
 
-
-      pet_1 = Pet.create({
+      @pet_1 = Pet.create({
         image: "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg",
         name: "Garfield",
         description: "Fat happy cat wants some lasagna.",
         age: 5,
         sex: "Male",
-        shelter_id: "#{shelter_1.id}"
+        shelter_id: "#{@shelter_1.id}"
         })
+    end
 
-      visit "/shelters/#{shelter_1.id}/pets"
+    it "then I see each Pet that can be adopted from that shelter with that shelter_id
+        including the pet's image, name, appoximate age, and sex" do
 
-      expect(page).to have_content("#{shelter_1.name}: Pets")
-      expect(page).to have_xpath("//img[contains(@src, '#{pet_1.image}')]")
-      expect(page).to have_content("#{pet_1.name}")
-      expect(page).to have_content(pet_1.age)
-      expect(page).to have_content("#{pet_1.sex}")
+      visit "/shelters/#{@shelter_1.id}/pets"
+
+      expect(page).to have_content("#{@shelter_1.name}: Pet")
+      expect(page).to have_xpath("//img[contains(@src, '#{@pet_1.image}')]")
+      expect(page).to have_content("#{@pet_1.name}")
+      expect(page).to have_content(@pet_1.age)
+      expect(page).to have_content("#{@pet_1.sex}")
 
       expect(page).to have_link("Create Pet")
     end
