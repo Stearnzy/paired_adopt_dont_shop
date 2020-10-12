c21c6f6a9411cc2a0a810dc4fef5d49ac8235c8d
Sage Gonzales <sagegonzales15@gmail.com>
2020-10-12 17:47:21 -0600

    Adds database for users controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_023452) do
+ActiveRecord::Schema.define(version: 2020_10_12_234533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 2020_10_10_023452) do
   create_table "shelters", force: :cascade do |t|
     t.string "name"
     t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "street_address"
     t.string "city"
     t.string "state"
     t.string "zip"
