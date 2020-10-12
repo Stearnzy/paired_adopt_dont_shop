c828cff4ba7f697824f61150647f770e81723efe
Zach Stearns <zach.stearns92@gmail.com>
2020-10-12 17:01:28 -0600

    Add @

--- app/controllers/shelter_pets_controller.rb
+++ app/controllers/shelter_pets_controller.rb
@@ -2,6 +2,6 @@ class ShelterPetsController < ApplicationController
   def index
     @shelter = Shelter.find(params[:id])
     # require "pry"; binding.pry
-    @pets = shelter.pets
+    @pets = @shelter.pets
   end
 end
