9a59c9ad4a87e03722faf19af524b4148d8995a7
Sage Gonzales <sagegonzales15@gmail.com>
2020-10-12 17:21:29 -0600

    Add test for user route

---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe "As a visitor" do
+  describe "when I visit a User's show page" do
+    before(:each) do 
+      @user = User.new({
+        name: 'Bobby',
+        street_address: '123 fake st.',
+        city: 'Fakertown',
+        state: 'CO',
+        zip: '80205'
+      })
+    end
+
+    it "I see all that User's information" do 
+      visit "/users/:id"
+
+      expect(page).to have_content("#{@user.name}")
+      expect(page).to have_content("#{@user.street_address}")
+      expect(page).to have_content("#{@user.city}")
+      expect(page).to have_content("#{@user.state}")
+      expect(page).to have_content("#{@user.zip}")
+    end
+    
+  end
+  
+  
+end
