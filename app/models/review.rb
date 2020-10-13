2402ac797ad51b968ca9f0c6c0a64ee0637ce321
Zach Stearns <zach.stearns92@gmail.com>
2020-10-13 12:26:04 -0600

    Add optional validation for picture

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ApplicationRecord
   belongs_to :shelter
   belongs_to :user
   validates_presence_of :title, :rating, :content, :user_name
+  # I think this is right..? We'll see...
+  validates_presence_of :picture, allow_nil: true
 end
