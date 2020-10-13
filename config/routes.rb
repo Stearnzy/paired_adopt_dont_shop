faca851745be3cac184dfea3efd0943b1bec5551
Zach Stearns <zach.stearns92@gmail.com>
2020-10-12 21:20:53 -0600

    Change order of user new and show

--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,8 @@ Rails.application.routes.draw do
   # Shelter Pets Controller path
   get '/shelters/:id/pets', to: 'shelter_pets#index'
 
-  # Users Controller path 
-  get '/users/:id', to: 'users#show'
+  # Users Controller path
   get '/users/new', to: 'users#new'
+  get '/users/:id', to: 'users#show'
   post '/users/:id', to: 'users#create'
 end
