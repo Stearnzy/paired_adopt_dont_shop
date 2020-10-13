987b0d1e5e7a09f0785eec4fa6d259c13492f3e0
Zach Stearns <zach.stearns92@gmail.com>
2020-10-12 21:20:39 -0600

    Change fill in syntax

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -6,18 +6,18 @@ describe "As a visitor" do
       visit "/users/new"
 
       fill_in "Name", with: "Harry Potter"
-      fill_in "Street Address", with: "555 Fluffytail Lane"
+      fill_in "Street address", with: "555 Fluffytail Lane"
       fill_in "City", with: "London"
       fill_in "State", with: "NY"
       fill_in "Zip", with: "80563"
 
       click_button("Create User")
       # expect(current_path).to  eq("/users/#{user.id}")
-      expect(page).to have_content("Harry Potter") 
-      expect(page).to have_content("555 Fluffytail Lane") 
-      expect(page).to have_content("London") 
-      expect(page).to have_content("NY") 
-      expect(page).to have_content("80563") 
+      expect(page).to have_content("Harry Potter")
+      expect(page).to have_content("555 Fluffytail Lane")
+      expect(page).to have_content("London")
+      expect(page).to have_content("NY")
+      expect(page).to have_content("80563")
     end
-  end 
+  end
 end
