63b35ecfa422e01a442fc5b246a30208da336c39
Sage Gonzales <sagegonzales15@gmail.com>
2020-10-12 22:23:55 -0600

    Add migrations for review relationships

---
+++ db/migrate/20201013040219_create_review.rb
@@ -0,0 +1,10 @@
+class CreateReview < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.string :title
+      t.integer :rating
+      t.string :content
+      t.string :user_name
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_234533) do
+ActiveRecord::Schema.define(version: 2020_10_13_041156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 2020_10_12_234533) do
     t.bigint "shelter_id"
     t.string "description"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.integer "rating"
+    t.string "content"
+    t.string "user_name"
+    t.bigint "shelter_id"
+    t.bigint "user_id"
+    t.index ["shelter_id"], name: "index_reviews_on_shelter_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "shelters", force: :cascade do |t|
@@ -42,4 +53,6 @@ ActiveRecord::Schema.define(version: 2020_10_12_234533) do
   end
 
   add_foreign_key "pets", "shelters"
+  add_foreign_key "reviews", "shelters"
+  add_foreign_key "reviews", "users"
 end
