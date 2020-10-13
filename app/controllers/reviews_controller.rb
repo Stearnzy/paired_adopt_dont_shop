01e8413e50c387c609809f8ddf30ea81870e17d3
Zach Stearns <zach.stearns92@gmail.com>
2020-10-13 12:49:47 -0600

    Controller and view edit for showing shelter review'

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,14 @@
 class ReviewController < ApplicationController
+  def new
+  end
 
-  def create 
-    @review = Review.create(review_params)
+  def create
+    @review = Review.create!(review_params)
+    redirect_to "/shelters/:shelter_id"
   end
 
   def review_params
     params.require(:title, :rating, :content, :user_name).permit(:picture)
   end
-  
+
 end
