753f681680136c80cad227954fc50827c44e96cb
Sage Gonzales <sagegonzales15@gmail.com>
2020-10-12 18:42:58 -0600

    Work in progress on user new test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,22 @@ class UsersController < ApplicationController
   def new 
 
   end
+
+  def create 
+    require 'pry'; binding.pry
+    user = User.create!({
+      name: params[:name],
+      street_address: params[:street_address],
+      city: params[:city],
+      state: params[:state],
+      zip: params[:zip]
+    })
+    require 'pry'; binding.pry
+
+    redirect_to "/users/#{user.id}"
+  end
   
   def show 
-    # require 'pry'; binding.pry
     @user = User.find(params[:id])
   end
 end
