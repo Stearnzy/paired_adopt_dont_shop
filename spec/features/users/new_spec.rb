77dd7761d869ff45f03dc86d6e48a58d4defe8af
Sage Gonzales <sagegonzales15@gmail.com>
2020-10-12 18:12:57 -0600

    Create test for User creation

---
+++ spec/features/users/new_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "As a visitor" do
+  describe "When I am taken to '/users/new'" do
+    it "I see a form for a new user to enter name and address" do
+      visit "/users/new"
+
+      fill_in "Name", with: "Harry Potter"
+      fill_in "Street Address", with: "555 Fluffytail Lane"
+      fill_in "City", with: "London"
+      fill_in "State", with: "NY"
+      fill_in "Zip", with: "80563"
+
+      click_button("Create User")
+      # expect(current_path).to  eq("/users/#{user.id}")
+      expect(page).to have_content("Harry Potter") 
+      expect(page).to have_content("555 Fluffytail Lane") 
+      expect(page).to have_content("London") 
+      expect(page).to have_content("NY") 
+      expect(page).to have_content("80563") 
+    end
+  end 
+end
