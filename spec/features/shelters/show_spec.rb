c077f8523b3b1331a5067062b16e4fa3c7eb9be1
Zach Stearns <zach.stearns92@gmail.com>
2020-10-13 21:04:27 -0600

    Comment out user story 4 and on:

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -56,51 +56,51 @@ describe "As a visitor" do
       expect(page).to have_link("To Shelters Index")
     end
 
-    it "I see a list of reviews for that shelter including
-     title, rating, content, user name, and optional picture." do
-     # With photo
-
-      visit "/shelters/#{@shelter.id}"
-
-      review = Review.create!({
-        title: "Great Place!",
-        rating: 4,
-        content: "Friendly staff, clean establishment",
-        user_name: "Cat Lady",
-        picture: "https://unsplash.com/photos/ethVHUKAaEI",
-        shelter_id: "#{@shelter.id}",
-        user_id: "#{@user.id}"
-      })
-
-      expect(page).to have_content("#{review.title}")
-      expect(page).to have_content("#{review.rating}")
-      expect(page).to have_content("#{review.content}")
-      expect(page).to have_content("#{review.user_name}")
-      expect(page).to have_content("#{review.picture}")
-    end
-
-    it "I see a list of reviews for that shelter including
-      title, rating, content, user name, and optional picture." do
-      # Without photo
-
-      visit "/shelters/#{@shelter.id}"
-
-      review = Review.create!({
-       title: "Great Place!",
-       rating: 4,
-       content: "Friendly staff, clean establishment",
-       user_name: "Cat Lady",
-       shelter_id: "#{@shelter.id}",
-       user_id: "#{@user.id}"
-       })
-
-       expect(page).to have_content("#{review.title}")
-       expect(page).to have_content("#{review.rating}")
-       expect(page).to have_content("#{review.content}")
-       expect(page).to have_content("#{review.user_name}")
-       expect(page).to_not have_content("#{review.picture}")
-    end
-
+    # it "I see a list of reviews for that shelter including
+    #  title, rating, content, user name, and optional picture." do
+    #  # With photo
+    #
+    #   visit "/shelters/#{@shelter.id}"
+    #
+    #   review = Review.create!({
+    #     title: "Great Place!",
+    #     rating: 4,
+    #     content: "Friendly staff, clean establishment",
+    #     user_name: "Cat Lady",
+    #     picture: "https://unsplash.com/photos/ethVHUKAaEI",
+    #     shelter_id: "#{@shelter.id}",
+    #     user_id: "#{@user.id}"
+    #   })
+    #
+    #   expect(page).to have_content("#{review.title}")
+    #   expect(page).to have_content("#{review.rating}")
+    #   expect(page).to have_content("#{review.content}")
+    #   expect(page).to have_content("#{review.user_name}")
+    #   expect(page).to have_content("#{review.picture}")
+    # end
+    #
+    # it "I see a list of reviews for that shelter including
+    #   title, rating, content, user name, and optional picture." do
+    #   # Without photo
+    #
+    #   visit "/shelters/#{@shelter.id}"
+    #
+    #   review = Review.create!({
+    #    title: "Great Place!",
+    #    rating: 4,
+    #    content: "Friendly staff, clean establishment",
+    #    user_name: "Cat Lady",
+    #    shelter_id: "#{@shelter.id}",
+    #    user_id: "#{@user.id}"
+    #    })
+    #
+    #    expect(page).to have_content("#{review.title}")
+    #    expect(page).to have_content("#{review.rating}")
+    #    expect(page).to have_content("#{review.content}")
+    #    expect(page).to have_content("#{review.user_name}")
+    #    expect(page).to_not have_content("#{review.picture}")
+    # end
+    #
     # it "I see a link to add a new reivew.  When I click this link
     #   I am taken to a new review path" do
     #   visit "/shelters/#{@shelter.id}"
