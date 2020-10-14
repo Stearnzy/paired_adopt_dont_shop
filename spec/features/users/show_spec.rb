032a226513323af9d535a2100c9d64ff94b133ac
Zach Stearns <zach.stearns92@gmail.com>
2020-10-13 20:59:04 -0600

    Remove whitespace

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "As a visitor" do
   describe "when I visit a User's show page" do
-    before(:each) do 
+    before(:each) do
       @user = User.create({
         name: 'Bobby',
         street_address: '123 fake st.',
@@ -12,7 +12,7 @@ describe "As a visitor" do
       })
     end
 
-    it "I see all that User's information" do 
+    it "I see all that User's information" do
       visit "/users/#{@user.id}"
 
       expect(page).to have_content("#{@user.name}")
@@ -21,8 +21,5 @@ describe "As a visitor" do
       expect(page).to have_content("#{@user.state}")
       expect(page).to have_content("#{@user.zip}")
     end
-    
   end
-  
-  
 end
