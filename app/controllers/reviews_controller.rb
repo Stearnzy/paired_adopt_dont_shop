de3eb9c681f5f4dd950c470ab396147dff381e52
Zach Stearns <zach.stearns92@gmail.com>
2020-10-14 19:28:25 -0600

    Add all functionality to make review editable; fix test syntax/grammar

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -22,4 +22,20 @@ class ReviewsController < ApplicationController
     @shelter = Shelter.find(params[:shelter_id])
     @review = Review.find(params[:review_id])
   end
+
+  def update
+    review = Review.find(params[:review_id])
+    review.update({
+      title: params[:title],
+      rating: params[:rating],
+      content: params[:content],
+      picture: params[:picture],
+      shelter_id: params[:shelter_id],
+      user_id: params[:user_id]
+      })
+
+    shelter_id = review.shelter_id
+
+    redirect_to "/shelters/#{shelter_id}"
+  end
 end
--- spec/features/reviews/edit_spec.rb
+++ spec/features/reviews/edit_spec.rb
@@ -2,16 +2,6 @@ describe "As a user" do
   describe "When I click on a review edit link" do
     it "I am taken to an edit shelter review page where I see a form that includes
     that review's pre populated title, rating, content, image, and user name" do
-    review = Review.create!({
-      title: "Great Place!",
-      rating: 4,
-      content: "Friendly staff, clean establishment",
-      user_name: "Cat Lady",
-      picture: "https://face4pets.org/wp-content/uploads/2015/06/shelter-cat2.jpg",
-      shelter_id: "#{@shelter.id}"
-      # user_id: "#{@user.id}"
-    })
-
     shelter = Shelter.create({
       name: "Crazy Cat Lady's",
       address: "123 Litterbox Way",
@@ -20,20 +10,30 @@ describe "As a user" do
       zip: "80110"
       })
 
+    review = Review.create!({
+      title: "Great Place!",
+      rating: 4,
+      content: "Friendly staff, clean establishment",
+      user_name: "Cat Lady",
+      picture: "https://face4pets.org/wp-content/uploads/2015/06/shelter-cat2.jpg",
+      shelter_id: "#{shelter.id}"
+      # user_id: "#{@user.id}"
+    })
+
     visit "/shelters/#{shelter.id}/review/#{review.id}/edit"
 
     expect(page).to have_content("Edit Review:")
-    expect(find_field('review[title]').value).to eq("#{review.title}")
-    expect(find_field('review[rating]').value).to eq("#{review.rating}")
-    expect(find_field('review[content]').value).to eq("#{review.content}")
-    expect(find_field('review[user_name]').value).to eq("#{review.user_name}")
-    expect(find_field('review[picture]').value).to eq("#{review.picture}")
+    expect(find_field('title').value).to eq("#{review.title}")
+    expect(find_field('rating').value).to eq("#{review.rating}")
+    expect(find_field('content').value).to eq("#{review.content}")
+    expect(find_field('user_name').value).to eq("#{review.user_name}")
+    expect(find_field('picture').value).to eq("#{review.picture}")
 
-    fill_in "review[rating]", with: 5
-    expect(find_field("review[rating]").value).to eq(5)
+    fill_in "rating", with: 5
+    expect(find_field("rating").value).to eq("5")
 
-    click_button('submit')
-    expect(current_path).to eq("/shelters/#{@shelter.id}")
+    click_button('Submit')
+    expect(current_path).to eq("/shelters/#{shelter.id}")
     end
   end
 end
