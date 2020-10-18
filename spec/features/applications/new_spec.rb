1fda80e34ef01c0b33849aa06a1cfdf511cbcd0f
Zach Stearns <zach.stearns92@gmail.com>
2020-10-17 23:19:29 -0600

    Add test for invalid username entered

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -14,12 +14,29 @@ describe "As a visitor" do
         })
       visit "/applications/new"
 
+      expect(page).to have_content("New Application")
+      expect(page).to have_content("Enter your username:")
       expect(page).to have_field("user_name")
 
       fill_in "user_name", with: "Bobby"
       click_on "Submit"
 
       expect(page).to have_content("In Progress")
+    end
+
+    it "I fill in the form with the name of a user that doesn't exist in the
+      database and click submit, I'm taken back to the new application page
+      and see a message that the user cannot be found" do
+      visit "/applications/new"
+
+      expect(page).to have_field("user_name")
+
+      fill_in "user_name", with: "Stewart"
+      click_on "Submit"
+
+      expect(page).to have_content("User cannot be found - please try again.")
+      expect(page).to have_content("New Application")
+      expect(page).to have_field("user_name")
     end
   end
 end
