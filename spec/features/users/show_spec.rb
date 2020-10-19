43b1623c95c619a0eda4a5348f783d31c4931283
Zach Stearns <zach.stearns92@gmail.com>
2020-10-18 21:42:47 -0600

    Create test for edge case where user has less than 2 reviews

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "As a visitor" do
   describe "when I visit a User's show page" do
     before(:each) do
-      @user = User.create({
+      @user_1 = User.create({
         name: 'Bobby',
         street_address: '123 fake st.',
         city: 'Fakertown',
@@ -36,53 +36,53 @@ describe "As a visitor" do
         })
 
       @review_1 = Review.create!({
-        user_name: "Bobby",
+        user_name: "#{@user_1.name}",
         title: "Love this place",
         rating: 5,
         content: "Great staff all around",
         shelter_id: "#{@shelter_1.id}",
-        user_id: "#{@user.id}"
+        user_id: "#{@user_1.id}"
         })
 
       @review_2 = Review.create!({
-        user_name: "Bobby",
+        user_name: "#{@user_1.name}",
         title: "Dogs are OK",
         rating: 2,
         content: "One peed on my leg",
         shelter_id: "#{@shelter_2.id}",
-        user_id: "#{@user.id}"
+        user_id: "#{@user_1.id}"
         })
 
       @review_3 = Review.create!({
-        user_name: "Bobby",
+        user_name: "#{@user_1.name}",
         title: "Bird crap everywhere",
         rating: 1,
         content: "Worst ever.",
         shelter_id: "#{@shelter_3.id}",
-        user_id: "#{@user.id}"
+        user_id: "#{@user_1.id}"
         })
     end
 
     it "I see all that User's information" do
-      visit "/users/#{@user.id}"
+      visit "/users/#{@user_1.id}"
 
-      expect(page).to have_content("#{@user.name}")
-      expect(page).to have_content("#{@user.street_address}")
-      expect(page).to have_content("#{@user.city}")
-      expect(page).to have_content("#{@user.state}")
-      expect(page).to have_content("#{@user.zip}")
+      expect(page).to have_content("#{@user_1.name}")
+      expect(page).to have_content("#{@user_1.street_address}")
+      expect(page).to have_content("#{@user_1.city}")
+      expect(page).to have_content("#{@user_1.state}")
+      expect(page).to have_content("#{@user_1.zip}")
     end
 
     it "I see the average rating of all of their reviews" do
-      visit "/users/#{@user.id}"
+      visit "/users/#{@user_1.id}"
 
-      expect(page).to have_content("Average User Rating: #{@user.review_average.round(2)}")
+      expect(page).to have_content("Average User Rating: #{@user_1.review_average.round(2)}")
     end
 
     describe "I see a section for Highlighted Reviews" do
       it "And I see the review with the best rating this user has written, and
         I see the review with the worst rating this user has written" do
-        visit "/users/#{@user.id}"
+        visit "/users/#{@user_1.id}"
 
         within "#highlighted" do
           expect(page).to have_content("Highlighted User Reviews")
@@ -100,6 +100,32 @@ describe "As a visitor" do
           expect(page).to have_content("#{@review_3.content}")
         end
       end
+    end
+
+    it "I do not see any highlighted reviews if user has less than two reviews" do
+
+      thomas = User.create({
+        name: 'Thomas',
+        street_address: "666 Lover's lane",
+        city: 'Little Paris',
+        state: 'NY',
+        zip: "96015"
+        })
+
+      review_4 = Review.create!({
+        user_name: "#{thomas.name}",
+        title: "Too much hair around",
+        rating: 2,
+        content: "Want to like it, but there's a lot",
+        shelter_id: "#{@shelter_2.id}",
+        user_id: "#{thomas.id}"
+        })
+
+      visit "/users/#{thomas.id}"
+
+      expect(page).to_not have_content("Highlighted User Reviews")
+      expect(page).to_not have_content("Best Review")
+      expect(page).to_not have_content("Worst Review")
     end
   end
 end
