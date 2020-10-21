e3986a935dbe1370d7d39244fc17e3cce731823a
Sage Gonzales <sagegonzales15@gmail.com>
2020-10-21 16:20:55 -0600

    Remove test for all approved method

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -91,45 +91,4 @@ describe Application, type: :model do
 
     expect(application_1.retrieve_user).to eq(user_1)
   end
-
-  xit "knows if all pet applications are approved" do
-    shelter_1 = Shelter.create({
-      name: "Crazy Cat Lady's",
-      address: "123 Litterbox Way",
-      city: "Littleton",
-      state: "CO",
-      zip: "80110"
-    })
-
-    user_1 = User.create({
-      name: 'Bobby',
-      street_address: '123 fake st.',
-      city: 'Fakertown',
-      state: 'CO',
-      zip: '80205'
-    })
-
-    pet_1 = Pet.create({
-      image: "https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074__340.jpg",
-      name: "Lizzie",
-      description: "Timid and affectionate once she knows you.",
-      age: 6,
-      sex: "female",
-      shelter_id: "#{shelter_1.id}",
-      adoptable: true
-    })
-
-    application_1 = Application.create(
-      user_id: "#{user_1.id}",
-      application_status: 'In Progress',
-    )
-
-    petapp_1 = PetApplication.create!(
-      application_id: "#{application_1.id}",
-      pet_id: "#{pet_1.id}",
-      approval: "Pending"
-    )
-
-    expect(application_1.all_approved?).to eq(true)
-  end
 end
