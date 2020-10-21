b4fddf46061105900faba78a78bf80c61b8f2772
Zach Stearns <zach.stearns92@gmail.com>
2020-10-21 16:08:33 -0600

    Write test on index page for deleting shelter with no approved applications

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -17,6 +17,59 @@ describe "As a visitor" do
             state: "NV",
             zip: "66666"
         })
+
+      @user = User.create({
+        name: 'Bobby',
+        street_address: '123 fake st.',
+        city: 'Fakertown',
+        state: 'CO',
+        zip: '80205'
+      })
+
+      @pet_1 = Pet.create({
+        image: "https://g5f3c5t9.rocketcdn.me/wp-content/uploads/2020/04/edited-vet-recommended-parakeet-sleeping-huts-1-of-1.jpg",
+        name: "Polly",
+        description: "She loves crackers",
+        age: 9,
+        sex: "Female",
+        shelter_id: "#{@shelter_2.id}"
+        })
+
+      @pet_2 = Pet.create({
+        image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ktvb.com%2Farticle%2Fentertainment%2Fplaces%2Fidaho-life%2Fidaho-life-boise-woman-finds-niche-as-pho-dog-grapher%2F277-d6456b15-6b2a-4bad-9b25-eeb58faf1323&psig=AOvVaw3hm5tZEVkZl17MKIwo9X_I&ust=1603338603299000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCODUzvHjxOwCFQAAAAAdAAAAABAD",
+        name: "Nena",
+        description: "She is always ready to share some of your food!",
+        age: 10,
+        sex: "female",
+        shelter_id: "#{@shelter_1.id}"
+      })
+
+      @pet_3 = Pet.create({
+        image: 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg',
+        name: "Garfield",
+        description: "Ready and willing to eat your lasagna.",
+        age: 13,
+        sex: "male",
+        shelter_id: "#{@shelter_1.id}"
+        })
+
+      @application_1 = Application.create!({
+        user_id: @user.id,
+        description: nil,
+        application_status: "Pending"
+        })
+
+      @petapp_2 = PetApplication.create!(
+        application_id: "#{@application_1.id}",
+        pet_id: "#{@pet_2.id}",
+        approval: "Approved"
+      )
+
+      @petapp_3 = PetApplication.create!(
+        application_id: "#{@application_1.id}",
+        pet_id: "#{@pet_3.id}",
+        approval: "Pending"
+      )
     end
 
     it "then I see the name of each shelter in the system" do
@@ -46,8 +99,8 @@ describe "As a visitor" do
         the link, I am returned to the Shelter Index Page" do
       visit '/shelters'
 
-      expect(page).to have_link("Delete", count: 2)
-      find(:xpath, "(//a[text()='Delete'])[2]").click
+      expect(page).to have_link("Delete Shelter", count: 2)
+      find(:xpath, "(//a[text()='Delete Shelter'])[2]").click
       expect(page).to_not have_content("#{@shelter_2.name}")
     end
 
@@ -65,6 +118,30 @@ describe "As a visitor" do
       visit "/shelters"
 
       expect(page).to have_link("To Pets Index")
+    end
+
+    it "If a shelter has approved applications for any of their pets I cannot
+        delete that shelter" do
+      visit "/shelters"
+
+      within "#shelter-#{@shelter_1.id}" do
+        click_link "Delete Shelter"
+      end
+
+      expect(page).to have_content("Cannot delete shelter with pending applications!")
+    end
+
+    it "If a shelter doesn't have any pets with an approved application I can delete
+        that shelter. When that shelter is deleted, all their pets are also deleted" do
+      visit "/shelters"
+
+      within "#shelter-#{@shelter_2.id}" do
+        click_link "Delete Shelter"
+      end
+
+      expect(page).to_not have_content("#{@shelter_2.name}")
+      click_link "To Pets Index"
+      expect(page).to_not have_content("#{@pet_1.name}")
     end
   end
 end
