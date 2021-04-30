6f08a9d55409153710fa57d57b7c80708dfa6c14
Zach Stearns <zach.stearns92@gmail.com>
2021-04-30 13:59:46 -0600

    Rubocop fixes

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Pet, type: :model do
-  describe "validations" do
+  describe 'validations' do
     it { should validate_presence_of :image }
     it { should validate_presence_of :name }
     it { should validate_presence_of :description }
@@ -10,72 +10,72 @@ describe Pet, type: :model do
     it { should validate_presence_of :adoptable }
   end
 
-  describe "relationships" do
+  describe 'relationships' do
     it { should belong_to :shelter }
     it { should have_many :pet_applications }
     it { should have_many(:applications).through(:pet_applications) }
   end
 
-  it "knows approved applications" do
+  it 'knows approved applications' do
     shelter_1 = Shelter.create({
-      name: "Crazy Cat Lady's",
-      address: "123 Litterbox Way",
-      city: "Littleton",
-      state: "CO",
-      zip: "80110"
-    })
+                                 name: "Crazy Cat Lady's",
+                                 address: '123 Litterbox Way',
+                                 city: 'Littleton',
+                                 state: 'CO',
+                                 zip: '80110'
+                               })
 
     user_1 = User.create({
-      name: 'Bobby',
-      street_address: '123 fake st.',
-      city: 'Fakertown',
-      state: 'CO',
-      zip: '80205'
-    })
+                           name: 'Bobby',
+                           street_address: '123 fake st.',
+                           city: 'Fakertown',
+                           state: 'CO',
+                           zip: '80205'
+                         })
 
     pet_1 = Pet.create({
-      image: "https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074__340.jpg",
-      name: "Lizzie",
-      description: "Timid and affectionate once she knows you.",
-      age: 6,
-      sex: "female",
-      shelter_id: "#{shelter_1.id}",
-      adoptable: true
-    })
+                         image: 'https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074__340.jpg',
+                         name: 'Lizzie',
+                         description: 'Timid and affectionate once she knows you.',
+                         age: 6,
+                         sex: 'female',
+                         shelter_id: shelter_1.id.to_s,
+                         adoptable: true
+                       })
 
     pet_2 = Pet.create({
-      image: "https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074__340.jpg",
-      name: "Nena",
-      description: "Cute, loving, and always hungry.",
-      age: 10,
-      sex: "female",
-      shelter_id: "#{shelter_1.id}",
-      adoptable: true
-    })
+                         image: 'https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074__340.jpg',
+                         name: 'Nena',
+                         description: 'Cute, loving, and always hungry.',
+                         age: 10,
+                         sex: 'female',
+                         shelter_id: shelter_1.id.to_s,
+                         adoptable: true
+                       })
 
     application_1 = Application.create(
-      user_id: "#{user_1.id}",
-      application_status: 'In Progress',
+      user_id: user_1.id.to_s,
+      application_status: 'In Progress'
     )
 
     petapp_1 = PetApplication.create!(
-      application_id: "#{application_1.id}",
-      pet_id: "#{pet_1.id}",
-      approval: "Pending"
+      application_id: application_1.id.to_s,
+      pet_id: pet_1.id.to_s,
+      approval: 'Pending'
     )
 
     application_2 = Application.create(
-      user_id: "#{user_1.id}",
-      application_status: 'Approved',
+      user_id: user_1.id.to_s,
+      application_status: 'Approved'
     )
 
     petapp_1 = PetApplication.create!(
-      application_id: "#{application_2.id}",
-      pet_id: "#{pet_2.id}",
-      approval: "Pending"
+      application_id: application_2.id.to_s,
+      pet_id: pet_2.id.to_s,
+      approval: 'Pending'
     )
 
     expect(pet_1.any_approved_applications?).to eq(false)
     expect(pet_2.any_approved_applications?).to eq(true)
-  end 
+  end
 end
